1a13ddf9346f82a524372b0de4c5270582916695
akamensi <akez.muhammed@gmail.com>
2024-07-30 01:01:56 +0200

    The Redux DevTools

--- src/store.js
+++ src/store.js
@@ -3,12 +3,16 @@ import { thunk } from "redux-thunk";
 
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
